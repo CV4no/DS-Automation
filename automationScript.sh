8770d5a2f7bc8944812474d643e6e6cc538cd759
SplunkAutobahnConsultants <92494749+SplunkAutobahnConsultants@users.noreply.github.com>
2021-10-20 10:53:15 -0400

    Update automationScript.sh

--- automationScript.sh
+++ automationScript.sh
@@ -18,23 +18,23 @@ bk=`cat ~/output02.txt |grep -i "already installed" |grep -o "already installed"
                            
                            fi
 
-echo ""
 
-echo "Checks if unzip is installed"
 
 echo ""
 
-sudo yum install unzip -y > ~/output01.txt
-bk=`cat ~/output01.txt |grep -i "already installed" |grep -o "already installed"`
+echo "Checks if git is installed"
+
+echo ""
+
+sudo apt install git-all -y > ~/output02.txt
+bk=`cat ~/output02.txt |grep -i "already installed" |grep -o "already installed"`
  if [ "$bk" == "already installed" ]
                 then 
-                           echo "The unzip package is $bk"
+                           echo "The git package is $bk"
                            
                            echo ""
                            
-                           echo "We are ok to unzip the file"
+                           echo "We are ok to download the file using git"
                            
                            fi
-
-
 
