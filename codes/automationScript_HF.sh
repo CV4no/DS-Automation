07f7d89557fb35986bcd3cbeb0cb3164c4413745
Bisrat Kebede <bsrimulugeta@gmail.com>
2021-10-20 17:23:23 -0400

    Update automationScript_HF.sh

--- codes/automationScript_HF.sh
+++ codes/automationScript_HF.sh
@@ -52,7 +52,7 @@ sudo tar -xvzf Splunk_TA_windows_DC.tar.gz  -C /opt/splunk/etc/deployment-apps
 #Removing uncessary directories & files
 sudo rm -r /opt/Splunk-DS-Automation/UF 
 sudo rm -r /opt/Splunk-DS-Automation/Deployment-Apps/Windows 
-cd /opt/Splunk-DS-Automation/
+cd /opt/Splunk-DS-Automation/codes/
 sudo rm -rf automationScript_HF.sh
 sudo rm -rf automationScript_UF.sh
 
