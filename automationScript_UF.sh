1b21e01a04eb6dbe51671ec20be496a398d539f2
SplunkAutobahnConsultants <92494749+SplunkAutobahnConsultants@users.noreply.github.com>
2021-10-20 15:14:51 -0400

    Create automationScript_UF.sh
    Automate necessary DS configs to UF which lets us communicate to DS.

---
+++ automationScript_UF.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+                                                #Version_01 by BK
+                                                
+#Splunk-DS-Automation by BK
+#Splunk Autobahn Delivery Engineering Team
+
+
+echo ""
+
+echo "Checks if git is installed"
+
+echo ""
+
+#git install
+sudo yum install git -y > ~/output.txt
+bk=`cat ~/output.txt |grep -i "already installed" |grep -o "Git is already installed"`
+ if [ "$bk" == "already installed" ]
+                then 
+                           echo "The git package is $bk"
+                           
+                           echo ""
+                           
+                           echo "We are ok to clone the file using git"
+                           
+                           fi
+                           
+   : ' git install on Ubuntu                          
+sudo  apt install git-install -y > ~/output.txt
+bk=`cat ~/output.txt |grep -i "already installed" |grep -o "Git is already installed"`
+ if [ "$bk" == "already installed" ]
+                then 
+                           echo "The git package is $bk"
+                           
+                           echo ""
+                           
+                           echo "We are ok to clone the file using git"
+                           
+                           fi
+    '   
+    
+#Cloning DS folder from SplunkAutobahnConsultants repo
+sudo git clone https://github.com/SplunkAutobahnConsultants/DS-Automation.git /opt/Splunk-DS-Automation
+
+#coping  necessary files to Splunk Deployment Apps directories
+cd /opt/Splunk-DS-Automation/UF/Linux/DS-Config/
+sudo tar -xvzf deploy-client-uf (1).tar.gz  -C /opt/splunkuniversalforwarder/etc/apps
+
+#Removing uncessary directories & files
+sudo rm -r /opt/Splunk-DS-Automation/UF/DC/DS-Config
+sudo rm -r /opt/Splunk-DS-Automation/Deployment-Apps
+cd /opt/Splunk-DS-Automation/
+sudo rm -rf automationScript_HF.sh
+sudo rm -rf automationScript_UF.sh
